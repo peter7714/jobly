357da83627e181bb207107749d38bdca31b483e8
peter <peter.26cast@gmail.com>
2023-04-06 21:03:30 -0400

    /

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,21 +1,24 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
-
+// makes selective update selective update queries
+// 
+// dataToUpdate is the new value you want to update
+// 
+// jsToSql maps javascript data fields into database column names
+// 
+// returns object {sqlSetCols, dataToUpdate} sqlSetCols is the column names, dataToUpdate is an array of the data
+// 
 // function to create an object with the column names to update in the db and how many values will be passed in // 
 function sqlForPartialUpdate(dataToUpdate, jsToSql)
 {
-  // extracts keys to update in the table for each column passed in // 
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  // sets name of column in the query and the amount of values that need to be passed in // 
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
-  // returns object with the columns to update in the db and their values //
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -2,7 +2,18 @@ const { BadRequestError } = require('../expressError');
 const { sqlForPartialUpdate } = require('./sql'); 
 
 describe('sqlForPartialUpdate', () => {
-    test('constructs proper object', () => {
+    test('constructs proper object with one value', () => {
+        const data = {
+            num_employees: 15,
+        };
+        const columns = {
+            numEmployees: "num_employees",
+        };
+        const {setCols, values} = sqlForPartialUpdate(data, columns);
+        expect(setCols).toEqual('\"num_employees\"=$1');
+        expect(values).toEqual([15]);
+    })
+    test('constructs proper object with two values ', () => {
         const data = {
             num_employees: 15,
             logo_url: 'img.com'
